083e898b86a8c550f57607fb11ce811062301316
teniolao <Tennybetty100@gmail.com>
2022-03-28 00:40:04 +0100

    level 1 complete

---
+++ src/Table.js
@@ -0,0 +1,37 @@
+import React from "react";
+import allCountryScores from "./scores";
+
+function Table() {
+  return (
+    <div>
+      <h1>High score per Country</h1>
+      <div>
+        {allCountryScores.map(({ name, scores }, index) => {
+          return (
+            <div key={index}>
+              <table>
+                <thead>
+                  <tr>
+                    <th scope="col">{name}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {scores.map(({ n, s }, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{n}</td>
+                        <td>{s}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Table;
---
+++ src/scores.js
@@ -0,0 +1,77 @@
+let allCountryScores = [
+  {
+    name: "Ethiopia",
+    scores: [
+      { n: "Sub", s: 9990 },
+      { n: "lucy", s: "4134234" },
+      { n: "DWH", s: 9999 },
+      { n: "Hanif", s: 999999999 },
+    ],
+  },
+  {
+    name: "Scotland",
+    scores: [
+      { n: "groundkeeper willie", s: 4000 },
+      { n: "Neill", s: 999999 },
+      { n: "braveheart", s: -200 },
+    ],
+  },
+  {
+    name: "England",
+    scores: [
+      { n: "Jonny", s: 202020 },
+      { n: "Chris", s: 202021 },
+    ],
+  },
+  {
+    name: "Brazil",
+    scores: [{ n: "Mozart", s: 999 }],
+  },
+  {
+    name: "Colombia",
+    scores: [
+      { n: "Maria", s: 6000 },
+      { n: "Melanie", s: "99999999" },
+      { n: "Ali", s: 5000 },
+    ],
+  },
+  {
+    name: "Turkey",
+    scores: [
+      { n: "selim", s: 900 },
+      { n: "mahmut", s: 1000 },
+      { n: "morat", s: 999 },
+    ],
+  },
+  {
+    name: "Iran",
+    scores: [
+      { n: "arosha", s: 5550 },
+      { n: "zahra", s: 3000 },
+      { n: "nader", s: 2000 },
+      { n: "Bani", s: 1999 },
+    ],
+  },
+  {
+    name: "Bangladesh",
+    scores: [
+      { n: "rahman", s: 700200 },
+      { n: "rayhan", s: 18238123 },
+      { n: "ali", s: 5400000 },
+    ],
+  },
+];
+
+
+// function answer(theN, theS) {
+//   for (let i = 0; i < allCountryScores.length; i++) {
+//   for (let j = 0; j < allCountryScores[i].scores.length; j++) {
+//      theN = allCountryScores[i].scores[j].n;
+//      theS = allCountryScores[i].scores[j].s;
+//   }
+// }
+// }
+
+//default {}
+
+export default allCountryScores;
